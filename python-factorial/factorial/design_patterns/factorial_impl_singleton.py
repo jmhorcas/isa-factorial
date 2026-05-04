8573032fc8362da79c4f04c20493227392e843df
José Miguel Horcas <horcas@uma.es>
2026-05-04 13:09:45 +0200

    Singleton en Python

--- python-factorial/factorial/design_patterns/factorial_impl_singleton.py
+++ python-factorial/factorial/design_patterns/factorial_impl_singleton.py
@@ -1,7 +1,15 @@
 from factorial import Factorial
-   
+
+class MetaSingleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(MetaSingleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
     
-class FactorialImpl(Factorial):
+    
+class FactorialImpl(Factorial, metaclass=MetaSingleton):
    
     def fact(self, n: int) -> int:
         if not isinstance(n, int):
